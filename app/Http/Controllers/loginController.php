fb1a6c4b0fc86c009bd21f51e6a83a278d07719b
RomanshkVolkov <chato4010@gmail.com>
2021-11-23 22:46:50 -0500

    prueba guzguz

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class loginController extends Controller
 {
     function index()
     {
+        //$users = User::paginate(20);
+
+        //return view('login.index', compact('users'));
         return view('login.index');
+    }
+
+       
+
+
+    public function store(){
+        if(auth()->attempt(request(['email', 'password']))==false){
+            return back()->withErrors([
+                'message'=>'The email or password is incorrect, please try again'
+            ]);
+        }
+        return redirect()->to('/');
+    }
+
+    public function destroy(){
+        auth()->logout();
+        return redirect()->to('/');
     }
 }
