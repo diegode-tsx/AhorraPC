e0d857ba7bd70ee1ab2fdc318fa6bcdd4682a7fe
DiegoG28 <diegogutcat28@gmail.com>
2021-11-25 19:33:01 -0500

    Página preferencias

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -7,11 +7,67 @@
 @endphp
 
 @section('title','Configuración')
-    
-@section('content')
-    <!---Cuerpo aqui puedes hacer la plantilla --->
-    <h1>Esta es la pagina de configuración</h1>
-    
-    
+@section('styles')
+<link rel="stylesheet" href="{{asset('css/setting.css')}}">   
 @endsection
 
+@section('logo')
+    <div class="logo">
+        <a href="{{route('home')}}">
+            <img src="{{asset('img/logo.png')}}" alt="">
+        </a>
+    </div>
+@endsection
+    
+@section('content')
+    <main>
+        <h1>Preferencias</h1>
+        <div class="content">
+            <div class="change-pass">
+                <div class="title">
+                    <i class="far fa-lock-open"></i>
+                    <h2>Cambiar contraseña</h2>
+                </div>
+                <form action="">
+                    <div class="input">
+                        <input type="password" name="actual-password" placeholder="Contraseña actual" required>
+                        <i class="fas fa-key"></i>
+                    </div>
+                    <div class="input">
+                        <input type="password" name="new_password" placeholder="Contraseña nueva" required>
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="input">
+                        <input type="password" name="new_password_confirmation" placeholder="Repetir contraseña" required>
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <input type="submit" value="Cambiar">
+                </form>
+            </div>
+
+            <div class="notification">
+                <div class="title">
+                    <i class="far fa-bell"></i>
+                    <h2>Notificaciones</h2>
+                </div>
+                <div class="checkbox">
+                    <label for="alert">
+                        Enviar notificaciones al correo cuando un producto marcado como favorito baje de precio
+                        <input type="checkbox" name="alert" id="alert">
+                        <div class="checkbox-box"></div>
+                    </label>
+                    
+                </div>
+            </div>
+
+            <div class="log-out">
+                <div class="title">
+                    <a href="#">
+                        <i class="far fa-power-off"></i>
+                        <h2>Cerrar sesión</h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
