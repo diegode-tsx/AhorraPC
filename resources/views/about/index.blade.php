707887d3e29d71e48dbabe6265741349a1b84645
DiegoG28 <diegogutcat28@gmail.com>
2021-11-28 19:38:05 -0500

    Página "Nosotros"

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,8 +1,45 @@
 @extends('layouts.plantilla-'.$plantilla)
 
-@section('title','Acerca de nosotros')
-    
+@php //borra el cache para que no entren dandole atras si no estan logueados
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+@endphp
+
+@section('title','Nosotros')
+@section('styles')
+<link rel="stylesheet" href="{{asset('css/about.css')}}">   
+@endsection
+
+@section('logo')
+    <div class="logo">
+        <a href="{{route('home')}}">
+            <img src="{{asset('img/logo.png')}}" alt="">
+        </a>
+    </div>
+@endsection
+
 @section('content')
-    <!---Cuerpo aqui puedes hacer la plantilla --->
-    <h1>Esta es la pagina de informacion de nosotros</h1>
+    <main>
+        <h1>AhorraPC es tu herramienta <span class="red">ideal</span> para ahorrar <span class="red">tiempo</span> y <span class="red">dinero</span></h1>
+
+        <div class="features-container">
+            <div class="feature first">
+                <img src="{{asset('img/hardware.png')}}" alt="">
+                <p>Busca componentes de computadoras en distintas páginas</p>
+            </div>
+            <div class="feature second">
+                <img src="{{asset('img/online-shopping.png')}}" alt="">
+                <p>Revisa los resultados de las tiendas de tu preferencia</p>
+            </div>
+            <div class="feature third">
+                <img src="{{asset('img/cheap.png')}}" alt="">
+                <p>Consigue los precios más bajos de la tienda</p>
+            </div>
+            <div class="feature fourth">
+                <img src="{{asset('img/favorite.png')}}" alt="">
+                <p>Guarda tus favoritos para comprar más tarde</p>
+            </div>
+        </div>
+    </main>
 @endsection
