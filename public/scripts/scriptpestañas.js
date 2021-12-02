239b8fbca180f29c2cb759294ed3a7847a718937
pedroahurom <angelpedro328@gmail.com>
2021-12-01 23:04:56 -0500

    peticionasyn

--- public/scripts/scriptpestañas.js
+++ public/scripts/scriptpestañas.js
@@ -184,4 +184,27 @@ function ActivarMenorPrecio(){
 
 
 
+function add_to_favorites(precio,nomProduct,linkImagen,linkCompra,tienda, token, ruta) {
+	
+	$.ajax({
+		url: ruta,
+		type: "POST",
+		data: {
+			precio: precio,//
+			nomProduct: nomProduct,//
+			linkImagen: linkImagen,
+			linkCompra: linkCompra,
+			tienda: tienda,//
+			_token: token,
+		},
+		success: function(data) {
+			if (data.success) {
+				console.log("Se agrego a favoritos");
+			}else{
+				
+			}
+		}
+
+	});
+}
 
